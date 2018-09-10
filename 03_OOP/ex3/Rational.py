128a96d665cf976450bd008051e6d511d8668ef4
Huynh Ha <huynhsamha@gmail.com>
2018-09-10 11:38:15 +0700

    update exercise 3

---
+++ 03_OOP/ex3/Rational.py
@@ -0,0 +1,27 @@
+import math
+
+class Rational:
+    def __init__(self, n = 0, d = 1):
+        assert(d != 0)
+        g = math.gcd(abs(n), abs(d))
+        self.numer = int(n / g)
+        self.denom = int(d / g)
+
+    def __str__(self):
+        return str(self.numer) + "/" + str(self.denom)
+
+    def __add__(self, another):
+        targetType = type(another).__name__
+        if targetType == 'int':
+            return self.addRational(Rational(another))
+        elif targetType == 'Rational':
+            return self.addRational(another)
+
+        raise Exception('Rational not support operator + with type ' + targetType)
+
+    def addRational(self, r):
+        assert(type(r).__name__ == 'Rational')
+        return Rational(
+            self.numer * r.denom + self.denom * r.numer,
+            self.denom * r.denom
+        )
---
+++ 03_OOP/ex3/run.py
@@ -0,0 +1,16 @@
+from Rational import Rational
+
+a = Rational(-12, 5)
+b = Rational(56, 64)
+
+print(str(a))
+print(str(b))
+
+c = a + b
+print(str(c))
+
+c = a + 13
+print(str(c))
+
+c = a + -21
+print(str(c))
