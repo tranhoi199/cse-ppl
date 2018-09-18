1872b59416212f37f591dbece7a50abc58c86812
Huỳnh Hà <huynhsamha@gmail.com>
2018-09-18 17:12:52 +0700

    add

--- 04_Functional-Programming/tut/main.py
+++ 04_Functional-Programming/tut/main.py
@@ -1,20 +1,10 @@
-from functools import reduce
-from itertools import repeat
+def lookup(pattern, lst, cmp):
+    arr = list(filter(lambda x: cmp(x) == pattern, lst))
+    return None if arr == [] else arr[0]
 
-def powRecursive(x, n):
-    return 1 if n == 0 else x * powRecursive(x, n-1)
+res = lookup("hello", 
+    # [('hello', 200), ('a', 50), ('he', 0), ('olleh', 500), ('olleh', 200), ('olleh', 100)],
+    [('hello', 200), ('a', 50), ('he', 0)],
+    lambda x: x[0][::-1]) # reversed
 
-print(powRecursive(1, 0))
-print(powRecursive(1, 1))
-print(powRecursive(1, 5))
-print(powRecursive(5, 0))
-print(powRecursive(5, 5))
-
-def powHoF(x, n):
-    return reduce(lambda a,b: a*b, list(repeat(x, n)), 1)
-
-print(powHoF(1, 0))
-print(powHoF(1, 1))
-print(powHoF(1, 5))
-print(powHoF(5, 0))
-print(powHoF(5, 5))
+print(res)
--- 04_Functional-Programming/tut/q2.py
+++ 04_Functional-Programming/tut/q2.py
@@ -2,7 +2,9 @@ from functools import reduce
 from itertools import repeat
 
 def powRecursive(x, n):
-    return 1 if n == 0 else x * powRecursive(x, n-1)
+    if n < 0 and x != 0: return powRecursive(1/x, -n)
+    elif n < 0: raise('Error')
+    return 1 if n >= 0 else x * powRecursive(x, n-1)
 
 print(powRecursive(1, 0))
 print(powRecursive(1, 1))
@@ -10,7 +12,14 @@ print(powRecursive(1, 5))
 print(powRecursive(5, 0))
 print(powRecursive(5, 5))
 
+print(powRecursive(-1, 0))
+print(powRecursive(-1, 1))
+print(powRecursive(-1, 5))
+print(powRecursive(0, 0))
+# print(powRecursive(0, -5))
+
 def powHoF(x, n):
+    if n < 0 and x != 0: return powHoF(1/x, -n)
     return reduce(lambda a,b: a*b, list(repeat(x, n)), 1)
 
 print(powHoF(1, 0))
