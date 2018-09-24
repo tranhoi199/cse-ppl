49300a3f64688fbaa0f2c5c333b9006a7898062b
Huỳnh Hà <huynhsamha@gmail.com>
2018-09-24 09:54:25 +0700

    add update tests

--- Assignments/assignment_2/src/test/ASTGenSuite_1537757632235.py
+++ Assignments/assignment_2/src/test/ASTGenSuite_1537757632235.py
@@ -854,140 +854,560 @@ var a: array[1 .. 2] of integer;
 
     def test_81(self):
         input = r"""
+procedure foo(
+    x: integer;
+    y, z: real;
+    g, h: string;
+    arr_nodes: Array[0 .. 1000] of real
+);
 
+var 
+    a, b, c: real;
+    p: boolean;
+    q: string;
+    i, j: integer;
+    dd: array[0 .. 1000005] of boolean;
+
+begin
+
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([FuncDecl(Id(r'foo'),[VarDecl(Id(r'x'),IntType()),VarDecl(Id(r'y'),FloatType()),VarDecl(Id(r'z'),FloatType()),VarDecl(Id(r'g'),StringType()),VarDecl(Id(r'h'),StringType()),VarDecl(Id(r'arr_nodes'),ArrayType(0,1000,FloatType()))],[VarDecl(Id(r'a'),FloatType()),VarDecl(Id(r'b'),FloatType()),VarDecl(Id(r'c'),FloatType()),VarDecl(Id(r'p'),BoolType()),VarDecl(Id(r'q'),StringType()),VarDecl(Id(r'i'),IntType()),VarDecl(Id(r'j'),IntType()),VarDecl(Id(r'dd'),ArrayType(0,1000005,BoolType()))],[],VoidType())]))
         self.assertTrue(TestAST.test(input, expect, 381))
 
     def test_82(self):
         input = r"""
+procedure foo();
+var a: real;
 
+begin
+    foo(2)[3+x] := a[b[f+y[2]]] + 3;
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([FuncDecl(Id(r'foo'),[],[VarDecl(Id(r'a'),FloatType())],[Assign(ArrayCell(CallExpr(Id(r'foo'),[IntLiteral(2)]),BinaryOp(r'+',IntLiteral(3),Id(r'x'))),BinaryOp(r'+',ArrayCell(Id(r'a'),ArrayCell(Id(r'b'),BinaryOp(r'+',Id(r'f'),ArrayCell(Id(r'y'),IntLiteral(2))))),IntLiteral(3)))],VoidType())]))
         self.assertTrue(TestAST.test(input, expect, 382))
 
     def test_83(self):
         input = r"""
+procedure foo();
+var a: real;
 
+begin
+    foo(2)[3+x] := a[b[f+y[2]-h[t[5+j]] * 4]] + 3;
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([FuncDecl(Id(r'foo'),[],[VarDecl(Id(r'a'),FloatType())],[Assign(ArrayCell(CallExpr(Id(r'foo'),[IntLiteral(2)]),BinaryOp(r'+',IntLiteral(3),Id(r'x'))),BinaryOp(r'+',ArrayCell(Id(r'a'),ArrayCell(Id(r'b'),BinaryOp(r'-',BinaryOp(r'+',Id(r'f'),ArrayCell(Id(r'y'),IntLiteral(2))),BinaryOp(r'*',ArrayCell(Id(r'h'),ArrayCell(Id(r't'),BinaryOp(r'+',IntLiteral(5),Id(r'j')))),IntLiteral(4))))),IntLiteral(3)))],VoidType())]))
         self.assertTrue(TestAST.test(input, expect, 383))
 
     def test_84(self):
         input = r"""
+procedure foo();
+var a: real;
 
+begin
+    a := not - F * G div 5 + (I or L and N + Y or Q * not -P) and 6 * 5 + O div not (5 mod T) ;
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([FuncDecl(Id(r'foo'),[],[VarDecl(Id(r'a'),FloatType())],[Assign(Id(r'a'),BinaryOp(r'+',BinaryOp(r'+',BinaryOp(r'div',BinaryOp(r'*',UnaryOp(r'not',UnaryOp(r'-',Id(r'F'))),Id(r'G')),IntLiteral(5)),BinaryOp(r'*',BinaryOp(r'and',BinaryOp(r'or',BinaryOp(r'+',BinaryOp(r'or',Id(r'I'),BinaryOp(r'and',Id(r'L'),Id(r'N'))),Id(r'Y')),BinaryOp(r'*',Id(r'Q'),UnaryOp(r'not',UnaryOp(r'-',Id(r'P'))))),IntLiteral(6)),IntLiteral(5))),BinaryOp(r'div',Id(r'O'),UnaryOp(r'not',BinaryOp(r'mod',IntLiteral(5),Id(r'T'))))))],VoidType())]))
         self.assertTrue(TestAST.test(input, expect, 384))
 
     def test_85(self):
         input = r"""
+procedure foo();
+var a: real;
 
+begin
+    a := (((5 <> 6) < (6 = 5)) >= (4 + 5 > 1)) <= 1 ;
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([FuncDecl(Id(r'foo'),[],[VarDecl(Id(r'a'),FloatType())],[Assign(Id(r'a'),BinaryOp(r'<=',BinaryOp(r'>=',BinaryOp(r'<',BinaryOp(r'<>',IntLiteral(5),IntLiteral(6)),BinaryOp(r'=',IntLiteral(6),IntLiteral(5))),BinaryOp(r'>',BinaryOp(r'+',IntLiteral(4),IntLiteral(5)),IntLiteral(1))),IntLiteral(1)))],VoidType())]))
         self.assertTrue(TestAST.test(input, expect, 385))
 
     def test_86(self):
         input = r"""
+procedure foo();
+var a: real;
+begin
+    if a = 1 then begin
+        if b > 3 then c := 5;
+        else d := 1;
 
+        if e < 4 then ok();
+    end else begin
+        if h > 5 then nty(); else lyo();
+        g := 5;
+    end
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([FuncDecl(Id(r'foo'),[],[VarDecl(Id(r'a'),FloatType())],[If(BinaryOp(r'=',Id(r'a'),IntLiteral(1)),[If(BinaryOp(r'>',Id(r'b'),IntLiteral(3)),[Assign(Id(r'c'),IntLiteral(5))],[Assign(Id(r'd'),IntLiteral(1))]),If(BinaryOp(r'<',Id(r'e'),IntLiteral(4)),[CallStmt(Id(r'ok'),[])],[])],[If(BinaryOp(r'>',Id(r'h'),IntLiteral(5)),[CallStmt(Id(r'nty'),[])],[CallStmt(Id(r'lyo'),[])]),Assign(Id(r'g'),IntLiteral(5))])],VoidType())]))
         self.assertTrue(TestAST.test(input, expect, 386))
 
     def test_87(self):
         input = r"""
-
+procedure foo();
+var a: real;
+begin
+    While i < 1 do begin
+        i := i+1;
+        if i = 1 then i := i - 1;
+    end
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([FuncDecl(Id(r'foo'),[],[VarDecl(Id(r'a'),FloatType())],[While(BinaryOp(r'<',Id(r'i'),IntLiteral(1)),[Assign(Id(r'i'),BinaryOp(r'+',Id(r'i'),IntLiteral(1))),If(BinaryOp(r'=',Id(r'i'),IntLiteral(1)),[Assign(Id(r'i'),BinaryOp(r'-',Id(r'i'),IntLiteral(1)))],[])])],VoidType())]))
         self.assertTrue(TestAST.test(input, expect, 387))
 
     def test_88(self):
         input = r"""
-
+procedure foo();
+var a: real;
+begin
+    for i := 1 to 10 do begin
+        for j := i downto 1 do
+            if (i + j) mod 2 = 1 then break;
+    end
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([FuncDecl(Id(r'foo'),[],[VarDecl(Id(r'a'),FloatType())],[For(Id(r'i'),IntLiteral(1),IntLiteral(10),True,[For(Id(r'j'),Id(r'i'),IntLiteral(1),False,[If(BinaryOp(r'=',BinaryOp(r'mod',BinaryOp(r'+',Id(r'i'),Id(r'j')),IntLiteral(2)),IntLiteral(1)),[Break()],[])])])],VoidType())]))
         self.assertTrue(TestAST.test(input, expect, 388))
 
     def test_89(self):
         input = r"""
-
+procedure foo();
+var a: real;
+begin
+    with 
+        a: integer;
+        b, c: array [0 .. 15] of Boolean;
+        x, y, z: real;
+    do begin
+        a := x := y := 3;
+    end
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([FuncDecl(Id(r'foo'),[],[VarDecl(Id(r'a'),FloatType())],[With([VarDecl(Id(r'a'),IntType()),VarDecl(Id(r'b'),ArrayType(0,15,BoolType())),VarDecl(Id(r'c'),ArrayType(0,15,BoolType())),VarDecl(Id(r'x'),FloatType()),VarDecl(Id(r'y'),FloatType()),VarDecl(Id(r'z'),FloatType())],[Assign(Id(r'a'),IntLiteral(3)),Assign(Id(r'x'),IntLiteral(3)),Assign(Id(r'y'),IntLiteral(3))])],VoidType())]))
         self.assertTrue(TestAST.test(input, expect, 389))
 
     def test_90(self):
         input = r"""
+var a, b, c: real;
 
+var x, y, z: Boolean;
+    g, h, y: Integer;
+
+function nty(): Real;
+var x, y, z: Integer;
+begin
+    readLine();
+    // This is readLine()
+
+    fs := readStdin();
+    
+    with i: integer; do begin
+        for i := 4 downto -5 do h := 6;
+        if i > 6 then return 0;
+    end
+
+    return 1;
+end
+
+var q, w : integer;
+
+function hgt(): Boolean;
+var a: string;
+begin 
+    (*
+        =======================================
+        Comment here
+        =======================================
+    *)
+end
 """
-        expect =str(Program([]))
+        expect =str(Program([VarDecl(Id(r'a'),FloatType()),VarDecl(Id(r'b'),FloatType()),VarDecl(Id(r'c'),FloatType()),VarDecl(Id(r'x'),BoolType()),VarDecl(Id(r'y'),BoolType()),VarDecl(Id(r'z'),BoolType()),VarDecl(Id(r'g'),IntType()),VarDecl(Id(r'h'),IntType()),VarDecl(Id(r'y'),IntType()),FuncDecl(Id(r'nty'),[],[VarDecl(Id(r'x'),IntType()),VarDecl(Id(r'y'),IntType()),VarDecl(Id(r'z'),IntType())],[CallStmt(Id(r'readLine'),[]),Assign(Id(r'fs'),CallExpr(Id(r'readStdin'),[])),With([VarDecl(Id(r'i'),IntType())],[For(Id(r'i'),IntLiteral(4),UnaryOp(r'-',IntLiteral(5)),False,[Assign(Id(r'h'),IntLiteral(6))]),If(BinaryOp(r'>',Id(r'i'),IntLiteral(6)),[Return(IntLiteral(0))],[])]),Return(IntLiteral(1))],FloatType()),VarDecl(Id(r'q'),IntType()),VarDecl(Id(r'w'),IntType()),FuncDecl(Id(r'hgt'),[],[VarDecl(Id(r'a'),StringType())],[],BoolType())]))
         self.assertTrue(TestAST.test(input, expect, 390))
 
     def test_91(self):
         input = r"""
+procedure foo();
+var a: real;
+begin
+    with 
+        a: integer;
+        b, c: array [0 .. 15] of Boolean;
+        x, y, z: real;
+    do begin
+        a := x := y := 3;
+    end
+end
 
+procedure foo();
+var a: real;
+begin
+    if a = 1 then begin
+        if b > 3 then c := 5;
+        else d := 1;
+
+        if e < 4 then ok();
+    end else begin
+        if h > 5 then nty(); else lyo();
+        g := 5;
+    end
+end
+
+function foo(): String;
+begin
+    ok(a);
+end
+
+var a:integer;
+var b:string;
+    f,g: real;
+var t,h: string;
+    p,q: boolean;
+
+procedure foo();
+begin
+    if True then begin
+        hic();
+        break;
+        continue;
+    end else return oh();
+end
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 391))
 
     def test_92(self):
         input = r"""
+var a, b, c: real;
 
+var x, y, z: Boolean;
+    g, h, y: Integer;
+
+function nty(): Real;
+var x, y, z: Integer;
+begin
+    readLine();
+    // This is readLine()
+
+    fs := readStdin();
+    
+    with i: integer; do begin
+        for i := 4 downto -5 do h := 6;
+        if i > 6 then return 0;
+    end
+
+    return 1;
+end
+
+var q, w : integer;
+
+function hgt(): Boolean;
+var a: string;
+begin 
+    (*
+        =======================================
+        Comment here
+        =======================================
+    *)
+end
+
+procedure foo();
+begin
+    if True then begin
+    end else begin end
+end
+
+function hgt(): Boolean;
+var a: string;
+begin 
+    (*
+        =======================================
+        Comment here
+        =======================================
+    *)
+end
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 392))
 
     def test_93(self):
         input = r"""
+Var Sel: String;
+    N1,N2, Total : Real;
+    YN : String;  { this is a character variable type, which holds single characters ONLY }
+procedure main();
+Begin
+	Total := 0;  { always initialise integer/real variables }
+	GotoXy(4,3);
+	Writeln("1.Addition");
+	GotoXy(4,4);
+	Writeln("2.Subtraction");
+	GotoXy(4,5);
+	Writeln("3.Exit");
+	GotoXy(6,8);
+	Write("Select: ");
+	Sel := Readkey();
 
+	If Sel = "1" {condition} Then 
+	Begin  {more than one statement}
+		ClrScr();
+		Write("Input No.1:");
+		Readln(N1);
+		Write("Input No.2:");
+		Readln(N2);
+		Total := N1 + N2;
+		Writeln("Addition: ",N1," + ",N2," = ",Total);
+		Write("Press any key to continue...");
+		Readkey();
+	end { Closing the if statement }
+
+	If Sel = "2" Then { note that here we do not use an assignment statement } 
+	Begin 
+		ClrScr();
+		Write("Input No.1:");
+		Readln(N1);
+		Write("Input No.2:");
+		Readln(N2);
+		Total := N1 - N2;
+		Write("Subtraction: ");
+		Write(N1," - ",N2," = ",Total);
+		Write("Press any key to continue...");
+		Readkey();
+	end  { Closing the if statement }
+
+	If Sel = "3" Then 
+	Begin
+		ClrScr();
+		Write("Are you sure?(Y/N)");
+		YN := Readkey();
+		If YN = "y" Then Halt(); { 1 instruction, so no need of Begin..End }
+		If YN = "n" Then Goto1(); { the goto statement is not recommended for frequent use }
+	End
+End
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 393))
 
     def test_94(self):
         input = r"""
+var
+   { local variable definition }
+   a : integer;
 
+procedure main();
+begin
+   a := 100;
+   (* check the boolean condition *)
+   if( a < 20 ) then
+      (* if condition is true then print the following *)
+      writeln("a is less than 20" );
+   
+   else
+      (* if condition is false then print the following *) 
+      writeln("a is not less than 20" );
+      writeln("value of a is : ", a);
+end
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 394))
 
     def test_95(self):
         input = r"""
+var
+exit: boolean;
 
+choice: integer;
+
+procedure main();
+begin
+   writeln("Do you want to continue? ");
+   writeln("Enter Y/y for yes, and N/n for no");
+   readln(choice);
+
+if(choice = "n") then
+   exit := true;
+else
+   exit := false;
+
+if (exit) then
+   writeln(" Good Bye!");
+else
+   writeln("Please Continue");
+
+readln();
+end
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 395))
 
     def test_96(self):
         input = r"""
-
+  VAR X, Y1, Y2,  First, Last, Incr, Factor: REAL;
+  Q1, Q2, Step:  INTEGER;
+  PROCEDURE main();
+  BEGIN
+  { Input plot  parameters }
+  Write("Enter  first value: ");
+  Read(First);
+  Write("Enter  last value: ");
+  Read(Last);
+  Write("Enter  scale factor: ");
+  Read(Factor);
+  Write("Enter an  increment: ");
+  Read(Incr);
+  WriteLn();
+  { Draw  horizontal Y axis }
+  FOR Step := 0 TO  MaxY DO
+  IF (Step MOD 5 =  0) THEN
+  Write("+");
+  ELSE
+  Write("-");
+  Write(" Y ");
+  WriteLn();
+  { Do the Plot on  its side }
+  X := First;
+  WHILE X <=  Last DO BEGIN
+  Y1 :=  SIN(3.14159 * X / 180.0);
+  Y1 := Factor *  Y1;
+  Q1 := ROUND(Y1);
+  Y2 := 0.005 * X;
+  Y2 := Factor *  Y2;
+  Q2 := ROUND(Y2);
+  FOR Step := 0 TO  MaxY DO
+  IF Step = 0 THEN
+  Write( "|");
+  ELSE
+  IF Step = Q1  THEN
+  Write( "*");
+  ELSE
+  IF Step = Q2  THEN
+  Write( "+");
+  ELSE
+  Write( " ");
+  WriteLn();
+  X := X + Incr;
+  END { WHILE }
+  Write("X");
+  END { SidePlotXY  }
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 396))
 
     def test_97(self):
         input = r"""
-
+  VAR Pennies:  INTEGER;
+  Tendered, Cost,  Remainder: REAL;
+  PROCEDURE Main();
+  BEGIN
+  (* Input  necessary information *)
+  Write("Enter cost  of item: ");
+  Read(Cost);
+  Write("Enter  amount tendered: ");
+  Read(Tendered);
+  (* Compute the  change in pennies *)
+  Remainder :=  Tendered - Cost;
+  Pennies := 0;
+  WHILE Remainder  > 0 DO BEGIN
+  Remainder :=  Remainder - 0.01;
+  Pennies :=  Pennies + 1;
+  END (* WHILE *)
+  (* Output all  required Results *)
+  Write("Cost is:  ");
+  Write(Cost);
+  Write(" Amount  tendered is: ");
+  Write(Tendered);
+  Write(" Change  is: ");
+  WriteLn(Pennies);
+  END (*  BadChanger *)
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 397))
 
     def test_98(self):
         input = r"""
-
+  (* Demonstrates  some String actions *)
+  (* that involve  names of people *)
+  VAR FirstName,  LastName, FullName: STRING;
+  Count,  NameCount, Gap: INTEGER;
+  PROCEDURE main();
+  BEGIN
+  Space := "  ";
+  Hyphen := "-";
+  Greeting :=  "Hello there ";
+  Write("Enter the  number of names: ");
+  ReadLn(NameCount);
+  WriteLn();
+  WHILE NameCount  >0 DO BEGIN
+  Write("Enter a  name, last name first: ");
+  Read(FullName);
+  Gap :=  POS(Space, FullName); { NOTE }
+  IF Gap > 0  THEN BEGIN
+  LastName :=  Copy(FullName, 1, Gap);
+  Delete(FullName,  1, Gap); { NOTE }
+  FirstName :=  FullName;
+  IF Length(LastName) <= 4 THEN
+  WriteLn("That is  a short last name");
+  IF Pos(Hyphen,  LastName) <> 0 THEN
+  WriteLn("That is  a hyphenated name");
+  IF FirstName =  "Bill" THEN { etc., etc. }
+  WriteLn("Bill is  a good name ");
+  FullName :=  FirstName + Space + LastName;
+  WriteLn(Greeting,  FullName);
+  WriteLn();
+  END { IF }
+  NameCount :=  NameCount - 1;
+  END { WHILE }
+  END { NameParse  }
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 398))
 
     def test_99(self):
         input = r"""
+Var
+	f : String; { file var of type byte }
+	sz : Integer;  { var for the size }
 
+Procedure Main();
+Begin
+	Assign(f,"C:\\\\anyfile.txt");
+	{$I-} Reset(f); {$I+}
+	If (IOResult <> 0) Then
+ 	Begin     { file found? }
+  		Writeln("File not found.. exiting");
+  		Readln();
+ 	End Else
+ 	Begin
+			{ Return the file size in Kilobytes }
+  		sz := Round(FileSize(f)/1024);
+  		Writeln("Size of the file in Kilobytes: ",sz," Kb");
+  		Readln();
+  		Close(f); 
+ 	End
+End
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 399))
 
     def test_100(self):
         input = r"""
+Var
+	NewDir : String; { for searching the dir and create a new one, if it does not exist }
+	F : String;
 
+Procedure Main();
+Begin
+	{ search for the dir }
+	NewDir := FSearch("C:\\\\Pascal Programming", GetEnv("")); 
+	{ create a new one, if it does not exist }
+	If NewDir = "" Then
+		CreateDir("C:\\\\Pascal Programming"); 
+	Assign(F,"C:\\\\Pascal Programming\\\\pascal-programming.txt");
+	{$I-} ReWrite(F); {$I+} { disable and enable back again I/O error checking } 
+	{ write to text file } 
+	Writeln(F,"http://pascal-programming.info/"); 
+	{$I-} Close(F); {$I+}
+End
 """
         expect =str(Program([]))
         self.assertTrue(TestAST.test(input, expect, 400))
